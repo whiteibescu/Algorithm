7ccb902fbfced861f6e00c0de3cd895eac6e921e
성수 박 <whiteibescu@gmail.com>
2021-07-06 20:04:33 +0900

    Correct Answers

--- BinarySearch2.py
+++ BinarySearch2.py
@@ -1,4 +1,5 @@
 import bisect
+
 def find(l, x):
     i = bisect.bisect_left(l, x)
     if i != len(l) and l[i] == x:
@@ -10,4 +11,4 @@ for _ in range(t):
     data = list(map(int, input().split()))
     query = list(map(int, input().split()))
     answer = [find(data, x) for x in query]
-    print(*ans)
+    print(*answer)
--- nk_mod_m.py
+++ nk_mod_m.py
@@ -1,26 +1,4 @@
-def Power(n, k, m):
-    if k == 0:
-        return 1
-    if k == 1:
-        return n
-
-    half = Power(n, k // 2, n)
-
-    if k & 2 == 0:
-        return (half * half) % m
-    else:
-
-        return (half * half * n) % n
-
-    # 재귀함수의 종료 조건
-    # 부분 문제를 해결 (재귀함수) npowerofk 아닌 더 작은 수를 구한다.
-
-    # 부분 문제를 모아 원래 문제를 해결
-    # 현재의 k가 짝수 일 때:
-    # 현재의 k가 홀수 일 때:
-
-
 t = int(input())
 for _ in range(t):
     n, k, m = map(int, input().split())
-    print(Power(n, k, m))
+    print(pow(n, k, m))
