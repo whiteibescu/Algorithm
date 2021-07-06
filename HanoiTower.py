666f730afeec82874f3e85878f1d6421c763864a
성수 박 <whiteibescu@gmail.com>
2021-07-06 19:58:11 +0900

    HanoiTower

--- HanoiTower.py
+++ HanoiTower.py
@@ -1,11 +1,14 @@
-def Hanoi(n, start, mid, end):
-    if n == 1:
-        return
-    Hanoi(n-1, start, end, mid)
-    Hanoi(start, '->', end)
-    Hanoi(n-1, mid, start, end)
+def hanoi(N, start, mid, end):
+    if N == 1:
+        print(f"{start} -> {end}")
+    else:
+        hanoi(N-1, start, end, mid)
+        hanoi(1, start, mid, end)
+        hanoi(N-1, mid, start, end)
+
 
 T = int(input())
-for _ in range(T):
-    n = int(input())
-    Hanoi(n, 'A', "B", "C")
+for test_case in range(1, T + 1):
+    N = int(input())
+
+    hanoi(N, 'A', 'B', 'C')
