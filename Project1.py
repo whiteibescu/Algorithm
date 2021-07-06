cdcbb0770e2dea400b509501d46a2e94240f7e7e
성수 박 <whiteibescu@gmail.com>
2021-07-06 15:49:07 +0900

    Project1

---
+++ Project1.py
@@ -0,0 +1,13 @@
+bracket = input()
+stack = []
+
+if len(bracket) % 2 == 0:
+    for i in bracket:
+        stack += i
+        
+
+
+    print("You're good")
+else:
+    print("No that's not how you do it")
+
